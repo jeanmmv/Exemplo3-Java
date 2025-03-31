49869d7995f43d3c03778102031a3671bb9c0fc9
Jean <jeancarlos12762@gmail.com>
2025-03-31 09:54:05 -0300

    operadores aritmeticos, logicos e comparacao

--- src/js/script.js
+++ src/js/script.js
@@ -1,52 +1,76 @@
-var nome = "FIAP";
-console.log(nome);
+// var nome = "FIAP";
+// console.log(nome);
 
-let idade = "19";
-console.log(idade);
+// let idade = "19";
+// console.log(idade);
 
-const sobrenome = "Faculdade";
-console.log(sobrenome);
+// const sobrenome = "Faculdade";
+// console.log(sobrenome);
 
-//declarando vaiáel indefinida
-let nome1;
-console.log(nome1);
+// //declarando vaiáel indefinida
+// let nome1;
+// console.log(nome1);
 
-//declarando variávl nula
-let nome2 = null;
-console.log(nome2);
+// //declarando variávl nula
+// let nome2 = null;
+// console.log(nome2);
 
-//tipos de variáveis
-let exemplo1 = 10;
-console.log(typeof exemplo1);
+// //tipos de variáveis
+// let exemplo1 = 10;
+// console.log(typeof exemplo1);
 
-let exemplo2 = "energia";
-console.log(typeof exemplo2);
+// let exemplo2 = "energia";
+// console.log(typeof exemplo2);
 
-let exemplo3 = {};
-console.log(typeof exemplo3);
+// let exemplo3 = {};
+// console.log(typeof exemplo3);
 
-let exemplo4 = {};
-console.log(typeof exemplo4);
+// let exemplo4 = {};
+// console.log(typeof exemplo4);
 
-//CONVERSÕES
+// //CONVERSÕES
 
-let numFloat = 1.233;
-console.log (parseInt (numFloat));
+// let numFloat = 1.233;
+// console.log (parseInt (numFloat));
 
-let numString = "123.456";
-console.log(parseFloat(numString));
+// let numString = "123.456";
+// console.log(parseFloat(numString));
 
-//METODOS
+// //METODOS
 
-//lenght = verifica o tamanho da string
-let frase = "O mundo e suas tecnologias"
-console.log(frase.length);
+// //lenght = verifica o tamanho da string
+// let frase = "O mundo e suas tecnologias";
+// console.log(frase.length);
 
-//indexOf - retorna um trecho do texto
+// //indexOf - retorna um trecho do texto
 
-let texto = "A programaçãp indomável e sustentável";
-console.log(texto.indexOf(""));
+// let texto = "A programaçãp indomável e sustentável";
+// console.log(texto.indexOf(""));
 
-//slice - retorna um trecho do texto com inicio e final
-let info = "processamento de ponta";
-console.log(info.slice(8,10))
+// //slice - retorna um trecho do texto com inicio e final
+// let info = "processamento de ponta";
+// console.log(info.slice(8,10));
+
+// OPERADORES ARITMETICOS
+
+const num1 = 10;
+const num2 = 20;
+
+console.log(num1+num2);
+console.log(num1-num2);
+console.log(num1*num2);
+console.log(num1/num2);
+
+//OPERADORES LÓGICOS
+
+const num3 = 20;
+const num4 = "20";
+
+console.log(num3 < num4);
+console.log(num3 < num4 && num4 < num3); //&& - Significa condição de "e"
+console.log(num3 < num4 || num4 < num3); //|| - significa condição de "ou"
+
+//OPERADORES DE COMPARAÇÃO
+
+console.log(num3 == num4);
+console.log(num3 === num4); //Compara variaveis de mesmo tipo
