c5cb28033859d842f9f5c7b5b7d59278697c4b17
Jean <jeancarlos12762@gmail.com>
2025-03-31 09:08:04 -0300

    Declarações e variaveis

---
+++ src/js/script.js
@@ -0,0 +1,18 @@
+var nome = "FIAP";
+console.log(nome);
+
+let idade = "19";
+console.log(idade);
+
+const sobrenome = "Faculdade";
+console.log(sobrenome);
+
+//declarando vaiáel indefinida
+let nome1;
+console.log(nome1);
+
+//declarando variávl nula
+let nome2 = null;
+console.log(nome2);
+
+
